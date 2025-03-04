c99da54499872a87ccb80d87d7c7e207180a199d
Antonio <antonio@engflow.com>
2025-03-04 17:39:18 +0000

    Add a grpc example with both server and client

---
+++ example/grpc/server.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+// TODO: use https://github.com/grpc/grpc/blob/master/examples/cpp/helloworld/greeter_server.cc
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
