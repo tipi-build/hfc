04c93b2731bd1beda56bf976a312b90b31be2969
Florent Castelli <florent@engflow.com>
2026-03-27 13:49:09 +0000

    Bump Goldilock minimum version to 1.2.1 in tests
    The previous version 1.2.0 had a few issues and would not build on more modern systems due to errors in tests.
    Those tests seem to test building goldilock from source, so the downloads must be invalid, which is why only the revision is updated.
    
    Change-Id: I411b89dc6eea36ac4ef18baac3bd64de98a558c3

--- test/test_project_templates/data/hfc_goldilock_base_value.cmake
+++ test/test_project_templates/data/hfc_goldilock_base_value.cmake
@@ -1,8 +1,9 @@
-set(GOLDILOCK_MINIMUM_VERSION 1.2.0)
+set(GOLDILOCK_MINIMUM_VERSION 1.2.1)
 hfc_ensure_goldilock_available(
-  GOLDILOCK_REVISION 5b34d2fed2d7da1280a2c5a8134f3d6fe10587df
+  GOLDILOCK_REVISION 5f8b9de72c10a6216c89a8807db8d420cff05512
   GOLDILOCK_MINIMUM_VERSION ${GOLDILOCK_MINIMUM_VERSION}
 
+  # The links and SHA1 are intentionally broken to force the tests to download and build goldilock from source
   GOLDILOCK_URL_PREBUILT_Darwin_arm64 https://github.com/tipi-build/goldilock/releases/download/v${GOLDILOCK_MINIMUM_VERSION}/goldilock-linux.zip
   GOLDILOCK_SHA_PREBUILT_Darwin_arm64 da1c7b9e9d79fa019ca0e41a002b6fed4fd854ee
 
